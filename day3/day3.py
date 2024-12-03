554df316bafbf9cf00cbb0b52b24e79ae7d7754d
JGoard <joshua.goard@hotmail.com>
2024-12-03 17:31:08 -0500

    initial commit for day 3. Completed the other part. I hate regex and it's silly magic

--- day3/day3.py
+++ day3/day3.py
@@ -1,32 +1,37 @@
+
+
 import sys
 import re
 import numpy as np
 
-pattern = re.compile("mul\([0-9]+,[0-9]+\)")
+pattern = re.compile("(mul\(\d{1,3},\d{1,3}\))|(do\(\))|(don't\(\))")
 pattern2 = re.compile("[0-9]+.*[0-9]+")
-pattern_do = re.compile("(?P<do>do\(\))+")
+
 
 result = []
 nums = []
-leftNum = []
-rightNum = []
 totalRes = 0
 leftorRight = False
 
 for i, line in enumerate(open("input.txt")):
-    only_do = re.split(r"[do(n'\t)()]", line)
-    print(only_do)
     for match in re.finditer(pattern, line):
-        # print(match.group())
+
+        print(match.group())
         result.append(match.group())
         # print("good num")
         
-        
+block = True
+
 for index in result:
-    for match in re.finditer(pattern2, index):
-        # print(match.group())
-        if match:
-            nums.append(match.group())
+    if(index == "do()"):
+        block = True
+    elif(index == "don't()"):
+        block = False
+    elif(block == True):
+        for match in re.finditer(pattern2, index):
+            # print(match.group())
+            if match:
+                nums.append(match.group())
 split_list = [string.split() for string in nums]
 splitted = []
 for v in nums:
